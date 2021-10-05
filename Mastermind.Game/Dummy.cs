f2bcde0d38583087dd79026b94d546fa06c53677
Nicolas Stohler <nstohler@gmail.com>
2021-10-05 14:02:53 +0200

    added code

---
+++ Mastermind.Game/Dummy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mastermind.Game
+{
+    public class Dummy
+    {
+        public int Add(int nr1, int nr2)
+        {
+            return nr1 + nr2;
+        }
+
+    }
+}
