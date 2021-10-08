258ed73d838f67713acd1693e2fe8d58126b533d
Nicolas Stohler <nstohler@gmail.com>
2021-10-08 10:12:51 +0200

    add sample WPF app

---
+++ WpfDemo/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfDemo.ViewModels;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Services
+            //services.AddSingleton<IFilesService, FilesService>();
+            //services.AddSingleton<ISettingsService, SettingsService>();
+            //services.AddSingleton<IClipboardService, ClipboardService>();
+            //services.AddSingleton<IShareService, ShareService>();
+            //services.AddSingleton<IEmailService, EmailService>();
+
+            // ViewModels
+            services.AddTransient<PersonViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ WpfDemo/ViewModels/PersonViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.ViewModels
+{
+    public class PersonViewModel : ObservableObject
+    {
+        public PersonViewModel()
+        {
+            FirstName = "Hans";
+            LastName = "Wurst";
+        }
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => SetProperty(ref _firstName, value);
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            set => SetProperty(ref _lastName, value);
+        }
+    }
+}
